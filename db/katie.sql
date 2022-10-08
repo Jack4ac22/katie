2f2c9a3e6b6b783d1b3fe75b7c52e0f2f23533c3
Jack Kazanjyan <kazanjian.jack.z@gmail.com>
2022-10-09 00:30:14 +0200

    sql

--- db/katie.sql
+++ db/katie.sql
@@ -1,9 +1,9 @@
--- phpMyAdmin SQL Dump
+-- phpMyAdmin SQL Dump s
 -- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Oct 08, 2022 at 02:38 AM
+-- Generation Time: Oct 08, 2022 at 10:28 PM
 -- Server version: 5.7.34
 -- PHP Version: 8.0.8
 
@@ -47,9 +47,18 @@ CREATE TABLE `groups` (
   `id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
   `description` varchar(255) NOT NULL,
-  `created_at` date NOT NULL,
-  `edited_at` date NOT NULL
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `edited_at` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `groups`
+--
+
+INSERT INTO `groups` (`id`, `title`, `description`, `created_at`, `edited_at`) VALUES
+(1, 'brave', 'some fighters', '2022-10-08 22:33:36', '2022-10-08 20:33:21'),
+(3, 'gladiators', 'Arena heros', '2022-10-08 22:33:36', '2022-10-08 20:33:21'),
+(4, 'sleeper', 'bed lovers who stay up late and sleep till noon.', '2022-10-08 22:34:51', '2022-10-08 20:34:17');
 
 -- --------------------------------------------------------
 
@@ -112,10 +121,42 @@ CREATE TABLE `people` (
   `first_name` varchar(255) NOT NULL,
   `last_name` varchar(255) NOT NULL,
   `sex` enum('male','female') NOT NULL,
-  `email` varchar(11) NOT NULL,
-  `created_at` date DEFAULT NULL,
-  `edited_at` date DEFAULT NULL
+  `email` varchar(255) NOT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
+  `edited_at` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `added_by` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `people`
+--
+
+INSERT INTO `people` (`id`, `first_name`, `last_name`, `sex`, `email`, `created_at`, `edited_at`, `added_by`) VALUES
+(11, 'jack', 'kazanjyan', 'male', 'jack@gmail.com', NULL, NULL, NULL),
+(12, 'Katie', 'Kazanjyan', 'female', 'katie@gmail.com', NULL, NULL, NULL),
+(13, 'Mark', 'dejong', 'male', 'mark@gmail.com', NULL, NULL, NULL),
+(14, 'Beth', 'De jong', 'female', 'beth@gmail.com', NULL, NULL, NULL),
+(15, 'matthew', 'dejong', 'male', 'matthew@gmail.com', NULL, NULL, NULL),
+(16, 'jack', 'kazanjyan', 'male', 'jack@gmail.com', NULL, NULL, NULL),
+(17, 'Katie', 'Kazanjyan', 'female', 'katie@gmail.com', NULL, NULL, NULL),
+(18, 'Mark', 'dejong', 'male', 'mark@gmail.com', NULL, NULL, NULL),
+(19, 'Beth', 'De jong', 'female', 'beth@gmail.com', NULL, NULL, NULL),
+(20, 'matthew', 'dejong', 'male', 'matthew@gmail.com', NULL, NULL, NULL),
+(23, 'asd', 'ddsd', 'male', 'asdad', '2022-10-08 00:00:00', NULL, NULL),
+(25, 'Joel', 'Myer', 'male', '', NULL, NULL, NULL),
+(26, 'Peter', 'Simpson', 'male', 'peter@email.com', '2022-10-08 00:00:00', NULL, NULL),
+(27, 'katie', 'de-jong', 'female', 'ktdjng@gmail.com', '2022-10-08 16:52:06', NULL, 16),
+(28, 'samantha', 'peterson', 'female', 'sam@gmail.com', '2022-10-08 16:52:43', NULL, 16),
+(29, 'Tony', 'Kazanjyan', 'male', 'tony@gmail.com', '2022-10-08 17:11:24', NULL, 16),
+(30, 'Kamala', 'Harris', 'female', 'kamala@gmail.com', '2022-10-08 17:13:35', NULL, 16),
+(31, 'Johny', 'doe', 'male', 'jj@gmail.com', '2022-10-08 17:22:43', NULL, 16),
+(32, 'Moulin', 'Smart', 'male', 'lkj@gmail.com', '2022-10-08 17:46:26', NULL, 16),
+(33, 'Kaleb', 'Namato', 'male', 'KK@gmail.com', '2022-10-08 17:47:19', NULL, 16),
+(34, 'stephen', 'stello', 'male', 'S@gmail.com', '2022-10-08 17:50:58', NULL, 16),
+(35, 'sstephen', 'stello', 'male', 'sS@gmail.com', '2022-10-08 17:51:45', NULL, 16),
+(36, 'sstephen', 'stello', 'male', 'ssS@gmail.com', '2022-10-08 17:52:21', NULL, 16),
+(37, 'pretty', 'Simpson', 'female', 'pella@gmail.com', '2022-10-08 17:56:02', NULL, 16),
+(38, 'Jemmy', 'smurf', 'male', 'jem@gmail.com', '2022-10-08 17:57:26', NULL, 16);
 
 -- --------------------------------------------------------
 
@@ -127,6 +168,22 @@ CREATE TABLE `people_groups` (
   `group_id` int(11) NOT NULL,
   `p_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `people_groups`
+--
+
+INSERT INTO `people_groups` (`group_id`, `p_id`) VALUES
+(1, 11),
+(1, 23),
+(1, 14),
+(1, 19),
+(3, 33),
+(3, 28),
+(3, 16),
+(3, 31),
+(4, 29),
+(4, 37);
 
 -- --------------------------------------------------------
 
@@ -161,10 +218,20 @@ CREATE TABLE `people_titles` (
 
 CREATE TABLE `phone_numbers` (
   `id` int(11) NOT NULL,
-  `number` int(11) NOT NULL,
+  `number` int(20) NOT NULL,
   `p_id` int(11) NOT NULL,
-  `description` varchar(255) NOT NULL
+  `description` varchar(255) NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `edited_at` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `phone_numbers`
+--
+
+INSERT INTO `phone_numbers` (`id`, `number`, `p_id`, `description`, `created_at`, `edited_at`) VALUES
+(1, 888718827, 11, 'personal', '2022-10-08 19:01:51', '2022-10-08 17:01:23'),
+(2, 332332332, 11, 'brother', '2022-10-08 19:01:51', '2022-10-08 17:01:23');
 
 -- --------------------------------------------------------
 
@@ -252,7 +319,8 @@ ALTER TABLE `languages`
 -- Indexes for table `people`
 --
 ALTER TABLE `people`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `added_by` (`added_by`);
 
 --
 -- Indexes for table `people_groups`
@@ -314,7 +382,7 @@ ALTER TABLE `comments`
 -- AUTO_INCREMENT for table `groups`
 --
 ALTER TABLE `groups`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `job_titles`
@@ -332,13 +400,13 @@ ALTER TABLE `languages`
 -- AUTO_INCREMENT for table `people`
 --
 ALTER TABLE `people`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 
 --
 -- AUTO_INCREMENT for table `phone_numbers`
 --
 ALTER TABLE `phone_numbers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `timezones`
@@ -361,6 +429,12 @@ ALTER TABLE `users`
 --
 ALTER TABLE `comments`
   ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`p_id`) REFERENCES `people` (`id`);
+
+--
+-- Constraints for table `people`
+--
+ALTER TABLE `people`
+  ADD CONSTRAINT `people_ibfk_1` FOREIGN KEY (`added_by`) REFERENCES `users` (`id`);
 
 --
 -- Constraints for table `people_groups`
