8f64335b61fceba9c1edc3b97e5fee3e2cf842b1
Jack Kazanjyan <kazanjian.jack.z@gmail.com>
2022-10-23 00:32:19 +0200

    textarea

--- app/views/pages/about.php
+++ app/views/pages/about.php
@@ -1,4 +1,10 @@
 <?php require_once APPROOT . '/views/includes/header.php'; ?>
 <h1 class="heading"><?php echo $data['title']; ?></h1>
 <p class="lead"><?php echo $data['description']; ?></p>
+
+<div class="form-floating">
+  <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style="height: 300px"></textarea>
+  <label for="floatingTextarea2">Comments</label>
+</div>
+
 <?php require_once APPROOT . '/views/includes/footer.php'; ?>
